742b2a14bfa7054abfc2305d006680b16d0cf83f
kumbasar <vkumbasar@outlook.com>
2017-06-11 04:25:54 +0300

    Update timeout

--- spec/api/MerkezBankasiKurXML_spec.js
+++ spec/api/MerkezBankasiKurXML_spec.js
@@ -16,9 +16,9 @@ frisby.create('XML stylesheet verification').waits(2000)
     .toss()
 
 //TC 3
-frisby.create('Verify that the response < 500 ms').waits(2000)
+frisby.create('Verify that the response < 1000 ms').waits(2000)
     .get(URL_TCMB)
-    .expectMaxResponseTime(500).expectStatus(200)
+    .expectMaxResponseTime(1000).expectStatus(200)
     .toss()
 
 //TC 4
